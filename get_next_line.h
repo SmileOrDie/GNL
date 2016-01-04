6f971cf25ad4f92c76f2a4dbc4b283cd854883d6
Smail HAMDANI <shamdani@e2r1p13.42.fr>
2016-01-04 15:30:20 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamdani <shamdani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/10 19:18:22 by shamdani          #+#    #+#             */
+/*   Updated: 2014/11/13 19:42:42 by shamdani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _GET_NEXT_LINE_H
+# define _GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int			get_next_line(int const fd, char **line);
+
+#endif
